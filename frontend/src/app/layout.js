359a4872e95851d520a94c0ae6f4cd19c15fd5ca
RohaanMooken <rohaanmooken@gmail.com>
2024-06-14 14:19:36 +0200

    Added support for dark mode based on system theme

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,17 +1,27 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/lib/themeProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "StarkHack",
-  description: "Website for StarkHack 2024",
+	title: "StarkHack",
+	description: "Website for StarkHack 2024",
 };
 
 export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={inter.className}>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
