2165da4ecd15f5118aaa0c980cdf8b21398baff1
RohaanMooken <rohaanmooken@gmail.com>
2024-06-15 10:48:50 +0200

    Super basic template overview of the create bounty page

--- frontend/src/app/bounties/create/page.js
+++ frontend/src/app/bounties/create/page.js
@@ -1,9 +1,29 @@
-import { EditorToggleBar } from "@/components/editorToggleBar";
+import { Editor } from "@/components/editor";
 
 export default function CreateBountyPage() {
-    return (
-        <div>
-            <EditorToggleBar />
-        </div>
-    );
+	return (
+		<div className="flex flex-col items-center space-y-8">
+			<h1>Create a Bounty</h1>
+			<p className="mb-4">
+				Please fill out the fields below accordingly to fit your
+				requirements.
+			</p>
+			<div className="flex flex-col items-center">
+				<h3>Program Overview</h3>
+				<Editor />
+			</div>
+			<div className="flex flex-col items-center">
+				<h3>Rewards by Threat Level</h3>
+				<Editor />
+			</div>
+			<div className="flex flex-col items-center">
+				<h3>Impacts in Scope</h3>
+				<Editor />
+			</div>
+			<div className="flex flex-col items-center">
+				<h3>Out of Scope & Rules</h3>
+				<Editor />
+			</div>
+		</div>
+	);
 }
