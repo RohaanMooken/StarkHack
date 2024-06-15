ccb01c9b687607d105e42a31c79d805c9776deb1
RohaanMooken <rohaanmooken@gmail.com>
2024-06-15 10:47:11 +0200

    Fix togglebar to work
    It works, however, we might want to revisit it later

--- frontend/src/components/editorToggleBar.jsx
+++ frontend/src/components/editorToggleBar.jsx
@@ -1,12 +1,11 @@
 "use client";
+
 import {
 	Bold,
 	Italic,
-	Underline,
 	Strikethrough,
-	AlignLeft,
-	AlignCenter,
-	AlignRight,
+    List,
+    ListOrdered,
 } from "lucide-react";
 import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
 import { useState } from "react";
@@ -14,9 +13,9 @@ import { useState } from "react";
 export function EditorToggleBar({ editor }) {
 	if (!editor) return null;
 
-	const [value, setValue] = useState("bold");
+	const [value, setValue] = useState(null);
 
-	function onChange(value) {
+	function switchValue(value) {
 		switch (value) {
 			case "bold":
 				editor.chain().focus().toggleBold().run();
@@ -24,33 +23,33 @@ export function EditorToggleBar({ editor }) {
 			case "italic":
 				editor.chain().focus().toggleItalic().run();
 				break;
-			case "underline":
-				editor.chain().focus().toggleUnderline().run();
-				break;
 			case "strikethrough":
 				editor.chain().focus().toggleStrike().run();
 				break;
-			case "alignleft":
-				editor.chain().focus().setTextAlign("left").run();
-				break;
-			case "aligncenter":
-				editor.chain().focus().setTextAlign("center").run();
-				break;
-			case "alignright":
-				editor.chain().focus().setTextAlign("right").run();
-				break;
 			case "h1":
-				editor.chain().focus().setHeading({ level: 1 }).run();
+				editor.chain().focus().toggleHeading({ level: 1 }).run();
 				break;
 			case "h2":
-				editor.chain().focus().setHeading({ level: 2 }).run();
+				editor.chain().focus().toggleHeading({ level: 2 }).run();
 				break;
 			case "h3":
-				editor.chain().focus().setHeading({ level: 3 }).run();
+				editor.chain().focus().toggleHeading({ level: 3 }).run();
 				break;
+            case "bulletlist":
+                editor.chain().focus().toggleBulletList().run();
+                break;
+            case "orderedlist":
+                editor.chain().focus().toggleOrderedList().run();
+                break;
 			default:
 				break;
 		}
+	}
+
+	function onChange(val) {
+		switchValue(value);
+		setValue(val);
+		switchValue(val);
 	}
 
 	return (
@@ -58,7 +57,6 @@ export function EditorToggleBar({ editor }) {
 			type="single"
 			value={value}
 			onValueChange={(value) => {
-				if (value) setValue(value);
 				onChange(value);
 			}}
 		>
@@ -71,20 +69,14 @@ export function EditorToggleBar({ editor }) {
 			<ToggleGroupItem value="italic">
 				<Italic className="h-4 w-4" />
 			</ToggleGroupItem>
-			<ToggleGroupItem value="underline">
-				<Underline className="h-4 w-4" />
-			</ToggleGroupItem>
 			<ToggleGroupItem value="strikethrough">
 				<Strikethrough className="h-4 w-4" />
 			</ToggleGroupItem>
-			<ToggleGroupItem value="alignleft">
-				<AlignLeft className="h-4 w-4" />
+            <ToggleGroupItem value="bulletlist">
+				<List className="h-4 w-4" />
 			</ToggleGroupItem>
-			<ToggleGroupItem value="aligncenter">
-				<AlignCenter className="h-4 w-4" />
-			</ToggleGroupItem>
-			<ToggleGroupItem value="alignright">
-				<AlignRight className="h-4 w-4" />
+            <ToggleGroupItem value="orderedlist">
+				<ListOrdered className="h-4 w-4" />
 			</ToggleGroupItem>
 		</ToggleGroup>
 	);
