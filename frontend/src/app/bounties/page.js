1ff67b892730a4410848f5eed292d5a520128e60
RohaanMooken <rohaanmooken@gmail.com>
2024-06-15 10:48:35 +0200

    Quick styling fixes for the editor

--- frontend/src/app/bounties/page.js
+++ frontend/src/app/bounties/page.js
@@ -4,7 +4,7 @@ import { DataTable } from "@/components/dataTable";
 export default function Bounties() {
     return(
         <main className="flex min-h-screen flex-col items-center p-24">
-			<h1 className="text-4xl">Bounties</h1>
+			<h1>Bounties</h1>
             <DataTable columns={columns} data={data} />
 		</main>
     );
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div className="flex flex-row items-center justify-between w-4/12">
 				<p className="text-lg text-muted">This is a small description explaining the site.</p>
-				<h1 className="text-4xl font-bold">Welcome to {siteConfig.name}</h1>
+				<h1 className="font-bold">Welcome to {siteConfig.name}</h1>
 			</div>
 		</main>
 	);
