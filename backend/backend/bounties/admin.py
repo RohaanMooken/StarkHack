f5f2073a31c278fdff18595d5c7441e9a5482681
RohaanMooken <rohaanmooken@gmail.com>
2024-06-15 14:36:46 +0200

    Create necessary models on the backend and register them on the admin site

--- backend/backend/bounties/admin.py
+++ backend/backend/bounties/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Bounty, BountyRewardCategory, BountyReward, BountyAssetInScope, BountyImpactInScope
 
-# Register your models here.
+class BountyRewardInline(admin.TabularInline):
+    model = BountyReward
+    extra = 1
+
+class BountyRewardCategoryInline(admin.TabularInline):
+    model = BountyRewardCategory
+    extra = 1
+    inlines = [BountyRewardInline]
+
+class BountyAssetInScopeInline(admin.TabularInline):
+    model = BountyAssetInScope
+    extra = 1
+
+class BountyImpactInScopeInline(admin.TabularInline):
+    model = BountyImpactInScope
+    extra = 1
+
+class BountyAdmin(admin.ModelAdmin):
+    exclude = ('last_updated',)
+    fieldsets = [
+        ('Bounty Information', {'fields': ['name', 'vault_tvl', 'max_bounty', 'total_paid']}),
+        ('Program Overview', {'fields': ['vault_address', 'program_overview']}),
+        ('Reward by Threat Level', {'fields': ['rewards_by_threat_level_json']}),
+        ('Assetes in Scope', {'fields': ['optional_assets_in_scope_json']}),
+        ('Impacts in Scope', {'fields': ['optional_impacts_in_scope_json']}),
+        ('Out of Scope', {'fields': ['out_of_scope_json']}),
+    ]
+    list_display = ('name', 'vault_tvl', 'max_bounty', 'total_paid', 'last_updated', 'vault_address', 'program_overview', 'rewards_by_threat_level_json', 'optional_assets_in_scope_json', 'optional_impacts_in_scope_json', 'out_of_scope_json')
+    inlines = [BountyRewardCategoryInline, BountyAssetInScopeInline, BountyImpactInScopeInline]
+
+admin.site.register(Bounty, BountyAdmin)
--- backend/backend/bounties/models.py
+++ backend/backend/bounties/models.py
@@ -1,3 +1,83 @@
 from django.db import models
 
 # Create your models here.
+class Bounty(models.Model):
+
+    # Bounty Information
+    name = models.CharField(max_length=100)
+    vault_tvl = models.FloatField()
+    max_bounty = models.FloatField()
+    total_paid = models.FloatField()
+    last_updated = models.DateField(auto_now=True)
+
+    # Program Overview
+    vault_address = models.CharField(max_length=100, )
+    program_overview = models.JSONField()
+
+    # Reward by Threat Level
+    rewards_by_threat_level_json = models.JSONField()
+
+    # Assetes in Scope
+    optional_assets_in_scope_json = models.JSONField()
+    
+    # Impacts in Scope
+    optional_impacts_in_scope_json = models.JSONField()
+    
+    # Out of Scope
+    out_of_scope_json = models.JSONField()
+
+    
+    def __str__(self):
+        return self.name
+
+
+class BountyRewardCategory(models.Model):
+    bounty = models.ForeignKey(Bounty, on_delete=models.CASCADE)
+    category = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category
+
+
+class BountyReward(models.Model):
+    bounty_reward_category = models.ForeignKey(BountyRewardCategory, on_delete=models.CASCADE)
+
+    threat_levels = [
+        ('Critical', 'Critical'),
+        ('High', 'High'),
+        ('Medium', 'Medium'),
+        ('Low', 'Low'),
+    ]
+
+    threat_level = models.CharField(max_length=100, choices=threat_levels, default='Low')
+    reward_start = models.FloatField(default=0.0)
+    reward_end = models.FloatField(default=0.0)
+
+    def __str__(self):
+        return self.threat_level
+
+
+class BountyAssetInScope(models.Model):
+    bounty = models.ForeignKey(Bounty, on_delete=models.CASCADE)
+    target = models.CharField(max_length=200, )
+    type = models.CharField(max_length=200, )
+
+    def __str__(self):
+        return self.target + ' - ' + self.type
+
+
+class BountyImpactInScope(models.Model):
+    bounty = models.ForeignKey(Bounty, on_delete=models.CASCADE)
+    
+    threat_levels = [
+        ('Critical', 'Critical'),
+        ('High', 'High'),
+        ('Medium', 'Medium'),
+        ('Low', 'Low'),
+    ]
+
+    threat_level = models.CharField(max_length=100, choices=threat_levels, default='Low')
+    impact = models.TextField()
+
+    def __str__(self):
+        return self.target + ' - ' + self.type
