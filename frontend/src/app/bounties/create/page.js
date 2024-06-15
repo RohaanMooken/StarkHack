73c72d94fa8210d6babbef2abf28ff5c33e490a3
RohaanMooken <rohaanmooken@gmail.com>
2024-06-15 11:45:02 +0200

    Fixes so lists in editor actually look like lists

--- frontend/src/app/bounties/create/page.js
+++ frontend/src/app/bounties/create/page.js
@@ -8,19 +8,19 @@ export default function CreateBountyPage() {
 				Please fill out the fields below accordingly to fit your
 				requirements.
 			</p>
-			<div className="flex flex-col items-center">
+			<div className="flex flex-col items-center w-full">
 				<h3>Program Overview</h3>
 				<Editor />
 			</div>
-			<div className="flex flex-col items-center">
+			<div className="flex flex-col items-center w-full">
 				<h3>Rewards by Threat Level</h3>
 				<Editor />
 			</div>
-			<div className="flex flex-col items-center">
+			<div className="flex flex-col items-center w-full">
 				<h3>Impacts in Scope</h3>
 				<Editor />
 			</div>
-			<div className="flex flex-col items-center">
+			<div className="flex flex-col items-center w-full">
 				<h3>Out of Scope & Rules</h3>
 				<Editor />
 			</div>
--- frontend/src/components/editor.jsx
+++ frontend/src/components/editor.jsx
@@ -11,13 +11,13 @@ export function Editor() {
 		],
         editorProps: {
             attributes: {
-                class: "border-white border-2 rounded-lg",
+                class: "border-white border-2 rounded-lg p-4",
             },
         },
 	});
 
 	return (
-		<div className="w-full">
+		<div className="w-6/12">
 			<EditorToggleBar editor={editor}/>
             <EditorContent editor={editor}/>
 		</div>
