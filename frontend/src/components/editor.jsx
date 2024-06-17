17ae8622db3aea8f4ac7da46adefbd4d5165e24e
RohaanMooken <rohaanmooken@gmail.com>
2024-06-17 19:39:06 +0200

    use callback functions to get editor html data to create page

--- frontend/src/components/editor.jsx
+++ frontend/src/components/editor.jsx
@@ -1,25 +1,30 @@
-"use client"
+"use client";
 
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { EditorToggleBar } from "@/components/editorToggleBar";
+import { useEffect } from "react";
 
-export function Editor() {
-    const editor = useEditor({
-		extensions: [
-			StarterKit,
-		],
-        editorProps: {
-            attributes: {
-                class: "border rounded-lg p-4",
-            },
-        },
+export function Editor({ setEditorContent }) {
+	const editor = useEditor({
+		extensions: [StarterKit],
+		editorProps: {
+			attributes: {
+				class: "border rounded-lg p-4",
+			},
+		},
+		// triggered on every change
+		onUpdate: ({ editor }) => {
+			const html = editor.getHTML()
+			// send the content to an API here
+			setEditorContent(html);
+		  },
 	});
-	
+
 	return (
 		<div className="w-6/12">
-			<EditorToggleBar editor={editor}/>
-            <EditorContent editor={editor}/>
+			<EditorToggleBar editor={editor} />
+			<EditorContent editor={editor} />
 		</div>
 	);
 }
