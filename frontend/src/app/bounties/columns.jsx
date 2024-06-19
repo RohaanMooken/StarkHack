3d8d7fe2d0543ce92db400138048fcb66d13ef81
RohaanMooken <rohaanmooken@gmail.com>
2024-06-19 07:28:57 +0200

    Update frontend accordingly to fetch bounties then display them on /bounties

--- frontend/src/app/bounties/columns.jsx
+++ frontend/src/app/bounties/columns.jsx
@@ -36,19 +36,19 @@ export const columns = [
             return <div className="text-center">{value}</div>;
         }
 	}),
-	columnHelper.accessor("vault_tvl", {
-		accessorKey: "vault_tvl",
-		header: () => <div className="text-center uppercase">Vault TVL</div>,
-		cell: ({ row }) => {
-			const amount = parseFloat(row.getValue("vault_tvl"));
-			const formatted = new Intl.NumberFormat("en-US", {
-				style: "currency",
-				currency: "USD",
-			}).format(amount);
+	// columnHelper.accessor("vault_tvl", {
+	// 	accessorKey: "vault_tvl",
+	// 	header: () => <div className="text-center uppercase">Vault TVL</div>,
+	// 	cell: ({ row }) => {
+	// 		const amount = parseFloat(row.getValue("vault_tvl"));
+	// 		const formatted = new Intl.NumberFormat("en-US", {
+	// 			style: "currency",
+	// 			currency: "USD",
+	// 		}).format(amount);
 
-			return <div className="text-center">{formatted}</div>;
-		},
-	}),
+	// 		return <div className="text-center">{formatted}</div>;
+	// 	},
+	// }),
 	columnHelper.accessor("max_bounty", {
 		accessorKey: "max_bounty",
 		header: () => <div className="text-center uppercase">Max Bounty</div>,
@@ -62,24 +62,24 @@ export const columns = [
 			return <div className="text-center">{formatted}</div>;
 		},
 	}),
-	columnHelper.accessor("total_paid", {
-		accessorKey: "total_paid",
-		header: () => <div className="text-center uppercase">Total Paid</div>,
-		cell: ({ row }) => {
-            const value = row.getValue("total_paid");
-			const amount = parseFloat(value);
-			const formatted = new Intl.NumberFormat("en-US", {
-				style: "currency",
-				currency: "USD",
-			}).format(amount);
+	// columnHelper.accessor("total_paid", {
+	// 	accessorKey: "total_paid",
+	// 	header: () => <div className="text-center uppercase">Total Paid</div>,
+	// 	cell: ({ row }) => {
+    //         const value = row.getValue("total_paid");
+	// 		const amount = parseFloat(value);
+	// 		const formatted = new Intl.NumberFormat("en-US", {
+	// 			style: "currency",
+	// 			currency: "USD",
+	// 		}).format(amount);
 
-            if (value === "Private") {
-                return <div className="text-center">Private</div>;
-            } else {
-                return <div className="text-center">{formatted}</div>;
-            }
-		},
-	}),
+    //         if (value === "Private") {
+    //             return <div className="text-center">Private</div>;
+    //         } else {
+    //             return <div className="text-center">{formatted}</div>;
+    //         }
+	// 	},
+	// }),
 	// TODO
 	// convert to nice datetime format
 	columnHelper.accessor("last_updated", {
@@ -99,7 +99,7 @@ export const columns = [
 			// Update for dyanmic bounty page
 			return (
 				<Link
-					href="/bounties"
+					href={`/bounties/${bounty.id}`}
 					className={buttonVariants({ variant: "outline" })}
 				>
 					<ChevronRight className="h-4 w-12" />
