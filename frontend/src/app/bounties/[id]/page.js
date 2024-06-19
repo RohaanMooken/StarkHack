c0bc0d1e1182063efd074e6527eae946e0915b26
RohaanMooken <rohaanmooken@gmail.com>
2024-06-19 20:27:19 +0200

    Display every bounty detail on bounty page

--- frontend/src/app/bounties/[id]/page.js
+++ frontend/src/app/bounties/[id]/page.js
@@ -1,12 +1,19 @@
 "use client";
 import { Component } from "react";
 import { siteConfig } from "@/config/site";
+import {
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+} from "@/components/ui/card";
 
 class BountyPage extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			data: [],
+			isLoading: true, // Add a loading state
 		};
 	}
 
@@ -16,21 +23,137 @@ class BountyPage extends Component {
 			.then((response) => response.json())
 			.then((data) => {
 				// Process the fetched data here
-				this.setState({ data });
-				console.log(data);
+				this.setState({ data, isLoading: false }); // Set loading to false when data is fetched
 			})
 			.catch((error) => {
 				// Handle any errors that occur during the fetch
 				console.log(error);
+				this.setState({ isLoading: false }); // Also set loading to false on error
 			});
 	}
 
 	render() {
 		const { params } = this.props; // Destructure params from props
+		const { data, isLoading } = this.state; // Destructure data and isLoading from state
+
+		if (isLoading) {
+			return <div>Loading...</div>; // Render a loading message or a spinner
+		}
+
 		return (
 			<div className="flex flex-col items-center space-y-20">
-				<h1>Bounty</h1>
-				<h2>{params.id}</h2>
+				<h1>{data.name}</h1>
+				<Card className="w-6/12">
+					<CardHeader className="flex flex-col items-center">
+						<h1>Program Overview</h1>
+					</CardHeader>
+					<CardContent>
+						<div
+							dangerouslySetInnerHTML={{
+								__html: data.program_overview_html,
+							}}
+						/>
+					</CardContent>
+				</Card>
+				<Card className="w-6/12">
+					<CardHeader className="flex flex-col items-center">
+						<h1>Rewards by Threat Level</h1>
+					</CardHeader>
+					<CardContent>
+						<div
+							dangerouslySetInnerHTML={{
+								__html: data.rewards_by_threat_level_html,
+							}}
+						/>
+					</CardContent>
+					<CardFooter>
+						{data.rewards.map((rewardCategory) => (
+							<Card className="w-full">
+								<CardHeader className="flex flex-col items-center">
+									<h3>{rewardCategory.category}</h3>
+								</CardHeader>
+								<CardContent className="flex flex-col items-center">
+									{rewardCategory.rewards.map((reward) => (
+										<div className="flex flex-row items-center justify-between w-full">
+											<p className="font-bold">
+												{reward.threat_level}
+											</p>
+											{reward.reward_start ==
+											reward.reward_end ? (
+												<p className="mr-20">
+													{reward.reward_start}
+												</p>
+											) : (
+												<p className="mr-20">
+													{reward.reward_start} -{" "}
+													{reward.reward_end}
+												</p>
+											)}
+										</div>
+									))}
+								</CardContent>
+							</Card>
+						))}
+					</CardFooter>
+				</Card>
+				<Card className="w-6/12">
+					<CardHeader className="flex flex-col items-center">
+						<h1>Assets in Scope</h1>
+					</CardHeader>
+					<CardContent>
+						<div
+							dangerouslySetInnerHTML={{
+								__html: data.optional_assets_in_scope_html,
+							}}
+						/>
+					</CardContent>
+					<CardFooter className="flex flex-col items-center">
+						{data.assets.map((asset) => (
+							<Card className="w-full">
+								<CardContent className="flex flex-row items-center justify-between w-full">
+									<p>{asset.target}</p>
+									<p className="mr-20">{asset.type}</p>
+								</CardContent>
+							</Card>
+						))}
+					</CardFooter>
+				</Card>
+				<Card className="w-6/12">
+					<CardHeader className="flex flex-col items-center">
+						<h1>Impacts in Scope</h1>
+					</CardHeader>
+					<CardContent>
+						<div
+							dangerouslySetInnerHTML={{
+								__html: data.optional_impacts_in_scope_html,
+							}}
+						/>
+					</CardContent>
+					<CardFooter className="flex flex-col items-center">
+						{data.impacts.map((impact) => (
+							<Card className="w-full">
+								<CardContent className="flex flex-row items-center w-full justify-between">
+									<p className="font-bold">
+										{impact.threat_level}
+									</p>
+									<p className="mr-20">{impact.impact}</p>
+								</CardContent>
+							</Card>
+						))}
+					</CardFooter>
+				</Card>
+				<Card className="w-6/12">
+					<CardHeader className="flex flex-col items-center">
+						<h1>Out of Scope</h1>
+					</CardHeader>
+					<CardContent>
+						<div
+							dangerouslySetInnerHTML={{
+								__html: data.out_of_scope_html,
+							}}
+						/>
+					</CardContent>
+				</Card>
 			</div>
 		);
 	}
