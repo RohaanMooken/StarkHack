b8ebbaed6c8191e80ba14572e2eb8fe754c6ed8c
RohaanMooken <rohaanmooken@gmail.com>
2024-06-20 15:15:29 +0200

    Update landingpage to look neater

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,19 +1,43 @@
 import { siteConfig } from "@/config/site";
 import Link from "next/link";
 import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Icons } from "@/components/icons";
+import {
+	PageHeader,
+	PageActions,
+	PageHeaderDescription,
+	PageHeaderHeading,
+} from "@/components/page-header";
 
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center space-y-20 p-24">
-			<div className="flex flex-row items-center justify-between w-4/12">
-				<p className="text-lg text-muted">
-					{siteConfig.name} is a fully anonymus bug bounty platform
-					designed to help developers find bugs and critical errors in
-					their Cairo and Starknet technologies.
-				</p>
-				<h1 className="font-bold">Welcome to {siteConfig.name}</h1>
-			</div>
-			<Link className={buttonVariants({ variant: "default" })} href={"/bounties"}>View Bounties</Link>
+			<PageHeader>
+				<PageHeaderHeading>
+					Welcome to {siteConfig.name}
+				</PageHeaderHeading>
+				<PageHeaderDescription>
+					Starkhack is a bug bounty and security services platform
+					dedicated to protecting crypto projects, with a special
+					focus on securing Cairo smart contract code from Web3
+					vulnerabilities.
+				</PageHeaderDescription>
+				<PageActions>
+					<Link href="/bounties" className={cn(buttonVariants())}>
+						Get Started
+					</Link>
+					<Link
+						target="_blank"
+						rel="noreferrer"
+						href={siteConfig.links.github}
+						className={cn(buttonVariants({ variant: "outline" }))}
+					>
+						<Icons.github className="mr-2 h-4 w-4" />
+						GitHub
+					</Link>
+				</PageActions>
+			</PageHeader>
 		</main>
 	);
 }
