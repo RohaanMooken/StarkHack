1f2a9f9dc85594b5366458d67f36a289fe6d4b65
RohaanMooken <rohaanmooken@gmail.com>
2024-06-20 14:27:11 +0200

    Update page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,12 +1,19 @@
 import { siteConfig } from "@/config/site";
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+		<main className="flex min-h-screen flex-col items-center space-y-20 p-24">
 			<div className="flex flex-row items-center justify-between w-4/12">
-				<p className="text-lg text-muted">This is a small description explaining the site.</p>
+				<p className="text-lg text-muted">
+					{siteConfig.name} is a fully anonymus bug bounty platform
+					designed to help developers find bugs and critical errors in
+					their Cairo and Starknet technologies.
+				</p>
 				<h1 className="font-bold">Welcome to {siteConfig.name}</h1>
 			</div>
+			<Link className={buttonVariants({ variant: "default" })} href={"/bounties"}>View Bounties</Link>
 		</main>
 	);
 }
