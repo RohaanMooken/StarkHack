2f5b31e0bcc3f9d28451c4bbeacc2c320fd51050
RohaanMooken <rohaanmooken@gmail.com>
2024-06-21 14:08:49 +0200

    Add check for wallet connected and post user address on report page

--- frontend/src/app/bounties/submit/[id]/page.js
+++ frontend/src/app/bounties/submit/[id]/page.js
@@ -6,13 +6,27 @@ import { Editor } from "@/components/editor";
 import { useEffect } from "react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
+import {
+	AlertDialog,
+	AlertDialogAction,
+	AlertDialogCancel,
+	AlertDialogContent,
+	AlertDialogDescription,
+	AlertDialogFooter,
+	AlertDialogHeader,
+	AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 
 export default function SubmitBountyPage({ params }) {
 	const [isLoading, setIsLoading] = useState(true);
 	const [data, setData] = useState([]);
 
 	const [editorContent, setEditorContent] = useState("");
-    const [pdf, setPdf] = useState("");
+	const [pdf, setPdf] = useState("");
+	
+	const [alertDialog, setAlertDialog] = useState(false);
+	const { setShowAuthFlow, primaryWallet } = useDynamicContext();
 
 	useEffect(() => {
 		fetch(`${siteConfig.backendURL}/bounties?id=${params.id}`)
@@ -31,20 +45,56 @@ export default function SubmitBountyPage({ params }) {
 	async function handleSubmit(e) {
 		e.preventDefault();
 
-        const formData = new FormData();
-        formData.append("short_description", editorContent);
-        formData.append("pdf", pdf);
+		if (!primaryWallet?.connected) {
+			setAlertDialog(true);
+			return;
+		}
 
-		const res = await fetch(`${siteConfig.backendURL}/bounties/?id=${params.id}`, {
-			method: "POST",
-			body: formData,
-		}).catch((err) => console.error(err));
+		const walletAddress = primaryWallet?.address;
+
+		const formData = new FormData();
+		formData.append("short_description", editorContent);
+		formData.append("pdf", pdf);
+		formData.append("owner_address", walletAddress);
+
+		const res = await fetch(
+			`${siteConfig.backendURL}/bounties/?id=${params.id}`,
+			{
+				method: "POST",
+				body: formData,
+			}
+		).catch((err) => console.error(err));
 	}
 
 	return isLoading ? (
 		<div>Loading...</div>
 	) : (
 		<div className="flex flex-col items-center space-y-20">
+			<AlertDialog open={alertDialog} onOpenChange={setAlertDialog}>
+				<AlertDialogContent>
+					<AlertDialogHeader>
+						<AlertDialogTitle>
+							Wallet not connected
+						</AlertDialogTitle>
+						<AlertDialogDescription>
+							To continue the bounty report creation process, please
+							connect your wallet below.
+						</AlertDialogDescription>
+						<AlertDialogFooter>
+							<AlertDialogCancel
+								onClick={() => (window.location.href = "/")}
+							>
+								Home
+							</AlertDialogCancel>
+							<AlertDialogAction
+								onClick={() => setShowAuthFlow(true)}
+							>
+								Connect Wallet
+							</AlertDialogAction>
+						</AlertDialogFooter>
+					</AlertDialogHeader>
+				</AlertDialogContent>
+			</AlertDialog>
 			<h1>Submit Bounty for {data.name}</h1>
 			<Card className="w-6/12">
 				<CardContent className="flex flex-col items-center space-y-8">
@@ -57,7 +107,12 @@ export default function SubmitBountyPage({ params }) {
 					<p>
 						Please upload a pdf file containing the full bug report.
 					</p>
-					<Input id="pdf" type="file" accept=".pdf" onChange={(e) => setPdf(e.target.files[0])}/>
+					<Input
+						id="pdf"
+						type="file"
+						accept=".pdf"
+						onChange={(e) => setPdf(e.target.files[0])}
+					/>
 					<Button onClick={(e) => handleSubmit(e)}>
 						Submit Report
 					</Button>
