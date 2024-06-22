2b37c8f03f989410c2e6aef2f621e7765111b3de
RohaanMooken <rohaanmooken@gmail.com>
2024-06-22 22:35:58 +0200

    Add logo to mainnav

--- frontend/src/components/mainNav.js
+++ frontend/src/components/mainNav.js
@@ -5,6 +5,9 @@ import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { siteConfig } from "@/config/site";
 import { useDynamicContext } from "@dynamic-labs/sdk-react-core";
+import Image from "next/image";
+import starkhack from "@/lib/starkhack.png";
+import logo from "@/lib/logo.png";
 
 export function MainNav() {
 	const pathName = usePathname();
@@ -14,9 +17,11 @@ export function MainNav() {
 		<div className="mr-4 hidden md:flex">
 			<Link href="/" className="mr-6 flex items-center space-x-2">
 				{/* Add Icon here if needed/wanted */}
+				<Image src={logo} alt="logo" className="h-6 w-6" />
 				<span className="hidden font-bold sm:inline-block text-2xl">
 					{siteConfig.name}
 				</span>
+				
 			</Link>
 			<nav className="flex items-center gap-4 text-sm lg:gap-6">
 				<Link
