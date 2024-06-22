ae78290e4f7d45be94f3ff079217604c71c7c1fe
Daniel Boye <danielboye888@gmail.com>
2024-06-22 21:39:03 +0200

    Landingpage UI update

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -4,40 +4,62 @@ import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { Icons } from "@/components/icons";
 import {
-	PageHeader,
-	PageActions,
-	PageHeaderDescription,
-	PageHeaderHeading,
+    PageHeader,
+    PageActions,
+    PageHeaderDescription,
 } from "@/components/page-header";
+import Image from "next/image";
+import starkhack from "@/lib/starkhack.png";
+import logo from "@/lib/logo.png";
 
 export default function Home() {
-	return (
-		<main className="flex min-h-screen flex-col items-center space-y-20 p-24">
-			<PageHeader>
-				<PageHeaderHeading>
-					Welcome to {siteConfig.name}
-				</PageHeaderHeading>
-				<PageHeaderDescription>
-					Starkhack is a bug bounty and security services platform
-					dedicated to protecting crypto projects, with a special
-					focus on securing Cairo smart contract code from Web3
-					vulnerabilities.
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center p-24">
+            <div className="flex items-end w-full relative">
+                <Image
+                    src={starkhack}
+                    alt="Starkhack"
+                    width={1000} // Adjust the width as needed
+                    className="neon-text m-20"
+                />
+                <div className="absolute inset-0 flex flex-col items-end justify-center pr-10">
+				<div className="flex flex-col items-center space-y-4 sm:space-y-6 md:space-y-8" style={{ position: 'relative', left: '-600px' }}> {/* Added inline styles */}
+						<Image
+                            src={logo}
+                            alt="logo"
+                            width={200}
+                            height={200}
+                            className="neon-text"
+                        />
+                        <PageHeaderDescription className="text-xl text-center">
+                            Protect and <br /> hack starknet
+                        </PageHeaderDescription>
+                        <PageActions className="flex justify-center">
+                            <Link href="/bounties" className={cn(buttonVariants(), "neon-button h-11 rounded-md px-8 text-base")}>
+                                Hack Now
+                            </Link>
+                        </PageActions>
+                    </div>
+                </div>
+            </div>
+            <PageHeader className="text-center">
+				<PageHeaderDescription className="pb-10 pt-10">
+					Starkhack is a bug bounty platform
+					dedicated to protecting crypto projects on starknet, with
+					focus on hacking Cairo smart contracts.
 				</PageHeaderDescription>
-				<PageActions>
-					<Link href="/bounties" className={cn(buttonVariants())}>
-						Get Started
-					</Link>
-					<Link
-						target="_blank"
-						rel="noreferrer"
-						href={siteConfig.links.github}
-						className={cn(buttonVariants({ variant: "outline" }))}
-					>
-						<Icons.github className="mr-2 h-4 w-4" />
-						GitHub
-					</Link>
-				</PageActions>
-			</PageHeader>
-		</main>
-	);
+                <PageActions className="mt-8 flex space-x-4 justify-center">
+                    <Link
+                        target="_blank"
+                        rel="noreferrer"
+                        href={siteConfig.links.github}
+                        className={cn(buttonVariants({ variant: "outline" }), "neon-button-outline")}
+                    >
+                        <Icons.github className="mr-2 h-4 w-4" />
+                        GitHub
+                    </Link>
+                </PageActions>
+            </PageHeader>
+        </main>
+    );
 }
