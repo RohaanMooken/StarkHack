430d2d41fa2c2fae7382232297a6d04f6c0f5dca
RohaanMooken <rohaanmooken@gmail.com>
2024-06-23 03:06:23 +0200

    Add nodeurl and testaddress

--- frontend/src/config/site.js
+++ frontend/src/config/site.js
@@ -3,5 +3,7 @@ export const siteConfig = {
     backendURL: "http://localhost:8000",
     links: {
         github: "https://github.com/RohaanMooken/starkhack/"
-    }
+    },
+    nodeURL: "https://starknet-sepolia.infura.io/v3/2TNLTC4uOBshElAudRJL42F1xC1",
+    testAddress: "0x0460b97a92060951b8bbdaf9bd5d68eec35a2bad8d7ec22e1e6a43705723e2af",
 }
